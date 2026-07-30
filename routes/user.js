efda9f574b53359991f6091d46cc135c1f2bb299
Aditya Pratap Singh <adpratap24@gmail.com>
2026-07-30 21:51:39 +0530

    login functionality added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router(); 
 const User = require("../models/user.js");
+const passport = require("passport");
+const wrapAsync = require("../utils/wrapAsync.js")
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -18,6 +20,15 @@ router.post("/signup", async(req,res)=>{
         req.flash("error",e.message);
         res.redirect("/signup");
     };
+});
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local",{failureRedirect: '/login', failureFlash:true}),async(req,res)=>{
+    req.flash("success","Welcome back to WonderVisit !");
+    res.redirect("/listings");
 });
 
 module.exports = router;
