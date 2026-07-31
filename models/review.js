e54550feeb3dbc009a9bddb51b682c22b5ceae9e
Aditya Pratap Singh <adpratap24@gmail.com>
2026-07-31 22:54:07 +0530

    author access

--- models/review.js
+++ models/review.js
@@ -11,7 +11,11 @@ const reviewSchema = new schema({
     createdAt: {
         type: Date,
         default: Date.now()
-    }
+    },
+    author: {
+        type:Schema.Types.ObjectId,
+        ref: "User",
+    },
  });
 
  module.exports = mongoose.model("Review",reviewSchema);  
--- routes/review.js
+++ routes/review.js
@@ -5,15 +5,18 @@ const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema,reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn} = require("../middleware.js");
 
 
 //post review route
-router.post("/", validateReview, wrapAsync(async(req,res)=>{
+router.post("/", validateReview,isLoggedIn, wrapAsync(async(req,res)=>{
     console.log(req.params.id);
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);   // fixed
+    newReview = new Review(req.body.review);
+    console.log(newReview);
     listing.reviews.push(newReview);
+    
     await newReview.save();
     await listing.save();
     req.flash("success", "new review created");
