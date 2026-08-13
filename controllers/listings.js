69fe5ada1eedc86a22c3bb878a091c4b019a30ce
Aditya Pratap Singh <adpratap24@gmail.com>
2026-08-14 00:13:37 +0530

    Preview Image Dimensions Adjusted

--- controllers/listings.js
+++ controllers/listings.js
@@ -89,7 +89,9 @@ module.exports.renderEditForm =async (req,res)=>{
         req.flash("error","listing you requested for does not exist");
         return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs",{listing});
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    res.render("listings/edit.ejs",{listing,originalImageUrl});
 };
 
 module.exports.updateListing = async(req,res)=>{
